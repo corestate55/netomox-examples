2b432a746f50485b2adc47469043aa97a48973f9
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-02-19 08:58:08 +0900

    add dep graph component

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,15 @@
 <template>
   <div id="app">
-    <div id="app-debug">
-      <ul>
-        <li>visualizer: {{ visualizer }}</li>
-        <li>modelFile: {{ modelFile }}</li>
-      </ul>
-    </div>
-    <div id="visualizer">
-      <!-- entry point -->
-    </div>
+    <DependencyGraph />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex'
-import DepGraphVisualizer from '../netoviz/src/dep-graph/visualizer'
-import '../netoviz/src/css/dep-graph.scss'
-
-const visualizer = new DepGraphVisualizer()
-
+import DependencyGraph from './components/DependencyGraph'
 export default {
   name: 'app',
-  methods: {
-    ...mapActions(['updateModelFile'])
-  },
-  computed: {
-    ...mapGetters(['visualizer', 'modelFile'])
-  },
-  mounted () {
-    const target = 'target3b.json'
-    this.updateModelFile(target)
-    visualizer.drawJsonModel(this.modelFile, null)
+  components: {
+    DependencyGraph
   }
 }
 </script>
---
+++ src/components/DependencyGraph.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+  <div id="app-debug">
+    <ul>
+      <li>visualizer: {{ visualizer }}</li>
+      <li>modelFile: {{ modelFile }}</li>
+    </ul>
+  </div>
+  <div id="visualizer">
+    <!-- D3.js entry point -->
+  </div>
+</div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import DepGraphVisualizer from '../../netoviz/src/dep-graph/visualizer'
+import '../../netoviz/src/css/dep-graph.scss'
+
+const visualizer = new DepGraphVisualizer()
+
+export default {
+  name: 'DependencyGraph.vue',
+  computed: {
+    ...mapGetters(['visualizer', 'modelFile'])
+  },
+  methods: {
+    ...mapActions(['updateModelFile'])
+  },
+  mounted () {
+    const target = 'target3b.json'
+    this.updateModelFile(target)
+    visualizer.drawJsonModel(this.modelFile, null)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
