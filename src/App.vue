249c13fe14c52ef1f65f6be43cbb8ca71e36b644
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-02-18 15:49:06 +0900

    add initial frontend code using dep-graph

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app-debug">
+      <ul>
+        <li>visualizer: {{ visualizer }}</li>
+        <li>modelFile: {{ modelFile }}</li>
+      </ul>
+    </div>
+    <div id="visualizer">
+      <!-- entry point -->
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapGetters, mapActions } from 'vuex'
+import DepGraphVisualizer from '../netoviz/src/dep-graph/visualizer'
+import '../netoviz/src/css/dep-graph.scss'
+
+const visualizer = new DepGraphVisualizer()
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  methods: {
+    ...mapActions(['updateModelFile'])
+  },
+  computed: {
+    ...mapGetters(['visualizer', 'modelFile'])
+  },
+  mounted () {
+    const target = 'target3b.json'
+    this.updateModelFile(target)
+    visualizer.drawJsonModel(this.modelFile, null)
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,61 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { json } from 'd3-request'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
   state: {
-
+    visualizer: 'Dependency',
+    modelFile: '',
+    wholeLayers: [],
+    selectedLayers: []
   },
   mutations: {
-
+    setVisualizer (state, payload) {
+      state.visualizer = payload
+    },
+    setModelFile (state, payload) {
+      state.modelFile = payload
+    },
+    setSelectedLayers (state, payload) {
+      state.selectedLayers = payload
+    },
+    setWholeLayers (state, payload) {
+      state.wholeLayers = payload
+    }
+  },
+  getters: {
+    visualizer (state) {
+      return state.visualizer
+    },
+    modelFile (state) {
+      return state.modelFile
+    },
+    selectedLayers (state) {
+      return state.selectedLayers
+    },
+    wholeLayers (state) {
+      return state.wholeLayers
+    }
   },
   actions: {
-
+    updateModelFile ({ commit, dispatch }, payload) {
+      commit('setModelFile', payload)
+      dispatch('initializeLayersFromModelFile')
+    },
+    initializeLayersFromModelFile ({ getters, commit }) {
+      const modelFile = getters.modelFile
+      json(`/draw/${modelFile}`, (error, modelData) => {
+        if (error) {
+          throw error
+        }
+        // graph object data to draw converted from topology json
+        const layers = modelData.map(d => d.name)
+        commit('setWholeLayers', layers)
+        commit('setSelectedLayers', layers)
+      })
+    }
   }
 })
