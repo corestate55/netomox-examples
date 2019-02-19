b470b4d8c2d1adc91539b585c0b6c42c7a07f910
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2019-02-19 11:46:05 +0900

    add watch timestamp and redraw graph in client

--- src/components/DependencyGraph.vue
+++ src/components/DependencyGraph.vue
@@ -1,10 +1,24 @@
 <template>
 <div>
-  <div id="app-debug">
-    <ul>
-      <li>visualizer: {{ visualizer }}</li>
-      <li>modelFile: {{ modelFile }}</li>
-    </ul>
+  <div class="debug">
+    <div>
+      app setting:
+      <ul>
+        <li>visualizer: {{ visualizer }}</li>
+        <li>modelFile: {{ modelFile }}</li>
+      </ul>
+    </div>
+    <div v-show="currentTimeStamp">
+      timestamps:
+      <ul>
+        <li
+          v-for="(value, key) in currentTimeStamp"
+          v-bind:key="key"
+        >
+          {{ key }} : {{ value }}
+        </li>
+      </ul>
+    </div>
   </div>
   <div id="visualizer">
     <!-- D3.js entry point -->
@@ -21,19 +35,52 @@ const visualizer = new DepGraphVisualizer()
 
 export default {
   name: 'DependencyGraph.vue',
+  data () {
+    return {
+      timer: null,
+      currentTimeStamp: null,
+      oldTimeStamp: null
+    }
+  },
   computed: {
     ...mapGetters(['visualizer', 'modelFile'])
   },
   methods: {
-    ...mapActions(['updateModelFile'])
+    ...mapActions(['updateModelFile']),
+    setModelUpdateCheckTimer () {
+      this.timer = setInterval(() => {
+        this.updateTimeStamp()
+        if (this.modelUpdated()) {
+          visualizer.drawJsonModel(this.modelFile)
+        }
+      }, 1500) // TODO: set interval
+    },
+    updateTimeStamp () {
+      this.oldTimeStamp = this.currentTimeStamp
+      const req = new XMLHttpRequest()
+      req.open('GET', '/watcher/timestamp', false)
+      req.onload = () => {
+        this.currentTimeStamp = JSON.parse(req.responseText)
+      }
+      req.send()
+    },
+    modelUpdated () {
+      return this.oldTimeStamp &&
+        this.oldTimeStamp.mtimeMs < this.currentTimeStamp.mtimeMs
+    }
   },
   mounted () {
-    const target = 'target3b.json'
+    const target = 'target3b.json' // TODO: model select, POST config server
     this.updateModelFile(target)
-    visualizer.drawJsonModel(this.modelFile, null)
+    visualizer.drawJsonModel(this.modelFile)
+    this.setModelUpdateCheckTimer()
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.debug {
+  background-color: lightgray;
+  padding: 1em;
+}
 </style>
