a3b1177260bace93473bb3a053b173a3c681302d
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2021-01-03 17:39:37 +0900

    Refactoring tinet_config class.

--- config_defs/topo2config_converter.rb
+++ config_defs/topo2config_converter.rb
@@ -3,17 +3,18 @@
 
 require 'optparse'
 require_relative './topo2config_converter_base'
+require_relative './tinet_config_ospf'
 
 # data converter (layer3 to tinet)
 class Topo2Layer3ConfigConverter < Topo2ConfigConverterBase
-  def initialize(file: '', debug: nil)
-    super(file: file, debug: debug)
-    construct_l3_config
+  def initialize(opts)
+    super(opts)
+    construct_layer3_config
   end
 
   private
 
-  def construct_l3_config
+  def construct_layer3_config
     l3_nw = @networks.find_network('layer3')
     l3_nw.nodes.each do |node|
       @tinet_config.add_l3_node(l3_nw, node)
@@ -25,8 +26,8 @@ end
 
 # data converter (ospf)
 class Topo2OSPFConfigConverter < Topo2Layer3ConfigConverter
-  def initialize(file: '', debug: nil)
-    super(file: file, debug: debug)
+  def initialize(opts)
+    super(opts)
     construct_ospf_config
   end
 
@@ -43,7 +44,7 @@ end
 # exec:
 # hagiwara@dev02:~/nwmodel/netomox-examples$ bundle exec ruby config_defs/topo2config_converter.rb
 
-opts = ARGV.getopts('debug:')
+opts = ARGV.getopts('', 'debug:')
 
 debug = opts['debug'] ? opts['debug'].intern : nil
 
@@ -53,8 +54,10 @@ file_path = file_dir.join(file_name).expand_path
 
 opts = { file: file_path, debug: debug }
 config_converter = if debug == :layer3
+                     opts[:tinet_config] = TinetConfigLayer3.new
                      Topo2Layer3ConfigConverter.new(opts)
                    else
+                     opts[:tinet_config] = TinetConfigOSPF.new
                      Topo2OSPFConfigConverter.new(opts)
                    end
 puts config_converter.to_config
