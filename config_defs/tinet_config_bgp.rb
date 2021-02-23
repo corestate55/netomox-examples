5d2df2363d5178425ece0e91378ca07207d37400
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2021-02-23 10:19:31 +0900

    WIP: add BGP base scripts.

---
+++ config_defs/tinet_config_bgp.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative './tinet_config_layer3'
+
+# Tinet config generator for bgp-proc topology model
+class TinetConfigBGP < TinetConfigLayer3
+  def add_bgp_node_config(node)
+    # TBA
+  end
+
+  def add_bgp_test(node)
+    # TBA
+  end
+end
