4afcee2cf76e89424373c9d3a91c926985add5a1
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2021-10-06 21:07:14 +0900

    Add mddo-pj trial layer3

--- model_defs/mddo.rb
+++ model_defs/mddo.rb
@@ -3,6 +3,7 @@ require 'netomox'
 require 'optparse'
 require_relative 'mddo/layer1'
 require_relative 'mddo/layer2'
+require_relative 'mddo/layer3'
 
 opts = ARGV.getopts('d')
 if opts['d']
@@ -13,5 +14,6 @@ end
 nws = Netomox::DSL::Networks.new
 register_target_layer1(nws)
 register_target_layer2(nws)
+register_target_layer3(nws)
 
 puts JSON.pretty_generate(nws.topo_data)
