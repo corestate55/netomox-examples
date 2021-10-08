c86429b9f26707ec2b70105e51ea5cf1c32516f6
corestate55 <manabu.hagiwara@okinawaopenlabs.org>
2021-10-08 15:31:30 +0900

    Add bgp-proc

--- model_defs/mddo.rb
+++ model_defs/mddo.rb
@@ -4,6 +4,7 @@ require 'optparse'
 require_relative 'mddo/layer1'
 require_relative 'mddo/layer2'
 require_relative 'mddo/layer3'
+require_relative 'mddo/bgp-proc'
 
 opts = ARGV.getopts('d')
 if opts['d']
@@ -15,5 +16,6 @@ nws = Netomox::DSL::Networks.new
 register_target_layer1(nws)
 register_target_layer2(nws)
 register_target_layer3(nws)
+register_target_bgp_proc(nws)
 
 puts JSON.pretty_generate(nws.topo_data)
